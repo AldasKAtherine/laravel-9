6137ef50677d4d89de7d2669c8570fe1df9fd1b6
AldasKAtherine <kaldas2401@uta.edu.ec>
2023-10-03 22:39:51 -0500

    eloquent

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PageController extends Controller
 {
@@ -10,15 +11,14 @@ class PageController extends Controller
         return view('home');
     }
     public function blog(){
-        $posts=[
-            ['id'=>1, 'title'=>'PHP', 'slug'=>'php'],
-            ['id'=>1, 'title'=>'laravel', 'slug'=>'laravel'],
-        ];
+       // $posts=Post::get();
+    //    $post = Post::first();
+    //    $post = Post::find(25);
+    //    dd($post);
+$posts = Post::latest()->paginate();
         return view('blog',['posts'=>$posts]);
     }
-    public function post($slug){
-        $post = $slug;
-
-      return view('post',['post'=>$post]);
+    public function post(Post $post){
+        return view('post',['post'=>$post]);
     }
 }
