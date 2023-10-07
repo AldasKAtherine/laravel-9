5a5b3ece9040bd8138597e5f688c2f459be44a0a
AldasKAtherine <kaldas2401@uta.edu.ec>
2023-10-06 22:45:19 -0500

    validations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,9 +21,14 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+        $request->validate([
+            'title'=>'required',
+            'slug'=>'required|unique:posts,slug',
+            'body'=>'required',
+        ]);
         $post = $request->user()->posts()->create([
             'title' =>$title =  $request->title,
-            'slug' => Str::slug($title),
+            'slug' => $request->slug,
             'body' =>$request->body,
         ]);
         return redirect()->route('posts.edit', $post);
@@ -32,6 +37,21 @@ class PostController extends Controller
     public function edit(Post $post){
         return view('posts.edit',['post'=> $post]);
     }
+
+    public function update(Request $request, Post $post){
+        $request->validate([
+            'title'=>'required',
+            'slug'=>'required|unique:posts,slug,'.$post->id,
+            'body'=>'required',
+        ]);
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' =>$request->body,
+        ]);
+        return redirect()->route('posts.edit', $post);
+    }
+
     public function destroy(Post $post){
         $post->delete();
         return back();
